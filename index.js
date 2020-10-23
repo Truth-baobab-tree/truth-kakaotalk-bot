353211b00d003cde484c11291c1a3f630558fa72
KHJcode <kimstudswcode4485@gmail.com>
2020-10-24 00:58:45 +0900

    origin master

--- index.js
+++ index.js
@@ -1,66 +1,20 @@
-const chatScreen = document.querySelector('.chatBox');
-const roomName = chatScreen.querySelector('h1');
-const submitForm = document.querySelector('.inputBox');
-const textBox = submitForm.querySelector('input');
-const button = submitForm.querySelector('button');
-
-class Replier {
-  reply(name) {
-    const chat = document.createElement('p');
-    chat.textContent = `Bot: ${name}`;
-    chatScreen.appendChild(chat);
-  }
-}
-
-function onSubmit(e) {
-  e.preventDefault();
-  const msg = textBox.value.trim();
-  textBox.value = '';
-  const room = roomName.textContent;
-  if (msg && msg !== '') {
-    const chat = document.createElement('p');
-    chat.textContent = `Person: ${msg}`;
-    chatScreen.appendChild(chat);
-    const replier = new Replier();
-    response(room, msg, 'Person', undefined, replier, undefined, undefined);
-  } else {
-    alert('채팅을 보낼 내용을 입력해주세요.');
-  }
-}
-
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName) {
-  if (
-    msg.indexOf('https://') !== -1 ||
-    msg.indexOf('http://') !== -1 ||
-    msg.indexOf('www.') !== -1
-  ) {
-    const url = msg;
-    const response = new Promise((resolve, reject) => {
-      const result = fetch('http://localhost:5050/page/get/score', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify({ url }),
-    });
-    setTimeout(async () => {
-      const data = (await result).json();
-      resolve(data);
-    }, [1000]);
-    });
+  if (room === '팀 바오밥나무') {
+    if (
+      msg.indexOf('https://') !== -1 ||
+      msg.indexOf('http://') !== -1 ||
+      msg.indexOf('www.') !== -1
+    ) {
 
-    response.then(result => {
-      replier.reply(`
-        공유된 웹사이트의 정보를
-        진실이라고 평가한 사용자는 ${result.truth}명,
-        거짓이라고 평가한 사용자는 ${result.lie}명입니다.
-      `);
-    });
+      const result = org.jsoup.Jsoup
+        .connect("https://truthserver.khjcode.repl.co/page/get/score")
+        .ignoreContentType(true)
+        .data({ url: msg })
+        .post();
+      const str = String(result);
+      const data = JSON.parse(str.substring(str.indexOf("{"), str.indexOf("}") + 1));
+
+      replier.reply('공유된 웹사이트의 정보를 진실이라고 평가한 사용자는 ' + data.truth + '명, 거짓이라고 평가한 사용자는 ' + data.lie + '명입니다.');
+    }
   }
 }
-
-function init() {
-  submitForm.addEventListener('submit', onSubmit);
-}
-
-init();
