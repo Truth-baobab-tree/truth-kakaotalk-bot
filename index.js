d869e773382de8cebec8e71d8429b7a680a209bb
KHJcode <kimstudswcode4485@gmail.com>
2020-11-20 00:51:59 +0900

    update

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName) {
-  if (room === '팀 바오밥나무') {
-    if (
-      msg.indexOf('https://') !== -1 ||
-      msg.indexOf('http://') !== -1 ||
-      msg.indexOf('www.') !== -1
-    ) {
+  if (
+    msg.indexOf('https://') !== -1 ||
+    msg.indexOf('http://') !== -1 ||
+    msg.indexOf('www.') !== -1
+  ) {
 
-      const result = org.jsoup.Jsoup
-        .connect("https://truthserver.khjcode.repl.co/page/get/score")
-        .ignoreContentType(true)
-        .data({ url: msg })
-        .post();
-      const str = String(result);
-      const data = JSON.parse(str.substring(str.indexOf("{"), str.indexOf("}") + 1));
+    const result = org.jsoup.Jsoup
+      .connect("https://truthserver.khjcode.repl.co/page/get/score")
+      .ignoreContentType(true)
+      .data({
+        url: msg
+      })
+      .post();
+    const str = String(result);
+    const data = JSON.parse(str.substring(str.indexOf("{"), str.indexOf("}") + 1));
 
-      replier.reply('공유된 웹사이트의 정보를 진실이라고 평가한 사용자는 ' + data.truth + '명, 거짓이라고 평가한 사용자는 ' + data.lie + '명입니다.');
-    }
+    replier.reply('공유된 웹사이트의 정보를 진실이라고 평가한 사용자는 ' + data.truth + '명, 거짓이라고 평가한 사용자는 ' + data.lie + '명입니다.');
   }
 }
